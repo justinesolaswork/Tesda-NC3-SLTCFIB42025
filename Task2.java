7d6aeedb029487e8d99e0c460d2ad9c86967c01e
Justine <justinesolas.work@gmail.com>
2025-08-29 10:10:21 +0800

    Initial commit

---
+++ Task2.java
@@ -0,0 +1,23 @@
+    //OUT PUT SHOULD BE H3ll0 w0rld 2.0 true
+    // Primitives Only Expect(long and double)
+public class Task2 {
+    public static void main(String[] args) {
+        char H = 'H';
+        int three = 3;
+        short one = 1;
+        short zero = 0;
+        char w = 'w';
+        char r = 'r';
+        char d = 'd';
+        float twoPointZero = 2.0F;
+        boolean bool = true;
+
+        String output = "" 
+            + H + three + one + one + zero + " "
+            + w + zero + r + one + d + " "
+            + twoPointZero + " "
+            + bool;
+
+        System.out.println(output);
+    }
+}
