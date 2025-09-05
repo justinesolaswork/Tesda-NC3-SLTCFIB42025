bd3b23fa44f2b1d7a9505310266a9f59525c282d
justinesolaswork <justinesolas.work@gmail.com>
2025-09-05 11:09:19 +0800

    Update task4.java

--- task4/task4.java
+++ task4/task4.java
@@ -1,15 +1,12 @@
 
-// import java.util.*;
+import java.util.*;
 
-// public class task4{
-//     public static void main(String[] args) {
-//         System.out.println("enter a word to check if Palindrome");
-//         Scanner input = new Scanner(System.in);
-//         String word = input.nextLine();
-//         String reverseversion = new StringBuilder(word).reverse().toString();
-//         System.out.println(word.equals(reverseversion) ? "Palindrome" : "Not a Palindrome" );
-//     }
-// }
-class P{ public static void main(String[] a){
-    System.out.println(a[0].equalsIgnoreCase(new StringBuilder(a[0]).reverse().toString())?"Palindrome":"Not a Palindrome");
-}}
+public class task4{
+    public static void main(String[] args) {
+        System.out.println("enter a word to check if Palindrome");
+        Scanner input = new Scanner(System.in);
+        String word = input.nextLine();
+        String reverseversion = new StringBuilder(word).reverse().toString();
+        System.out.println(word.equals(reverseversion) ? "Palindrome" : "Not a Palindrome" );
+    }
+}
